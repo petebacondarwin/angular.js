4f16626b1fcef5b0e60a784c1e5879a53da7f7ff
Martin Staffa <mjstaffa@googlemail.com>
2015-02-28 17:46:20 +0100

    test(ngJq): add expectations, add test for specific version of jq

--- test/e2e/tests/ngJqSpec.js
+++ test/e2e/tests/ngJqSpec.js
@@ -1,14 +1,12 @@
-// Sample E2E test:
-//
-describe('Force jqlite', function() {
-  beforeEach(function() {
+describe('Customizing the jqlite / jquery version', function() {
+
+  it('should be able to force jqlite', function() {
     loadFixture("ngJq").andWaitForAngular();
+    expect(browser.executeScript('return window.angular.element !== window.jQuery')).toBe(true);
   });
 
-  it('should use jqlite', function() {
-    expect(element(by.binding('text')).getText())
-        .toBe('Hello, world!');
-
-    // expect(browser.executeScript('return window.angular.jq();')).toBe('');
+  it('should be able to use a specific version jQuery', function() {
+    loadFixture("ngJqJquery").andWaitForAngular();
+    expect(browser.executeScript('return window.angular.element !== window.jQuery_2_0_0')).toBe(true);
   });
 });
